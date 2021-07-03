677ca2edebcb9aac480980cb202cb2371b53bad2
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-03 10:23:03 +0800

    added exercise 2.9 phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'   
-  }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   const checkDupe = (arr, test) => {
     let found = arr.find(element => element.name === test.name)
@@ -30,24 +33,49 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
-  const Display = ({ name, number }) => {
+  const handleFilterChange = (event) => {
+    //console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  const Display = ( {name, number} ) => {
     return (
       <li>{name} {number}</li>
+    )
+  }
+
+  const Filter = () => {
+    const checkIncl = (name, test) => {
+      if (name.toLowerCase().includes(test.toLowerCase())) {
+        return name
+      }
+    }
+    return (
+    persons
+    .filter(obj => checkIncl(obj.name, newFilter))
+    .map(person => <Display key={person.name} name={person.name} number={person.number} />)
     )
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input 
+        value={newFilter}
+        onChange={handleFilterChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNew}> 
         <div>
           name: <input 
@@ -67,9 +95,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <Display key={person.name} name={person.name} number={person.number} />
-        )}
+        <Filter />
       </ul>
     </div>
   )
