21c409efb4ca1b15e4728f4b40a5fc3086fb6534
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-04 14:36:14 +0800

    added exercise 2.19 phonebook step11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,11 +4,24 @@ import Filter from './components/Filter';
 import Form from './components/Form';
 import personService from './services/person'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     personService
@@ -61,10 +74,16 @@ const App = () => {
             setPersons(persons.map(person => person.id !== id ? person : returnedNotes))
             setNewName('')
             setNewNumber('')
-            window.alert(`${newName}'s number was changed`)
-          })
+            setErrorMessage(`${newName}'s number was changed`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            })
       } else {
-        window.alert(`${newName}'s number was not changed`)
+        setErrorMessage(`${newName}'s number was not changed`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
       }
     } else {
         personService
@@ -73,6 +92,10 @@ const App = () => {
             setPersons(persons.concat(returnedNotes))
             setNewName('')
             setNewNumber('')
+            setErrorMessage(`${newName}'s number was added`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
           })
     }
   }
@@ -80,6 +103,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage}/>
       <div>
         filter shown with <input 
         value={newFilter}
