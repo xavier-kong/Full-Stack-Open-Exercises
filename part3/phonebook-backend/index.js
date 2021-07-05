83f349ed8def643b665c776446fed8b674d5e31c
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-05 16:54:52 +0800

    added exercise 3.6 phonebook backend step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -61,14 +61,29 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  }
-  persons = persons.concat(person)
 
-  response.json(persons)
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name is missing' 
+    })
+  } else if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number is missing' 
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  } else {
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    }
+    persons = persons.concat(person)
+  
+    response.json(persons)
+  }
 })
 
 
