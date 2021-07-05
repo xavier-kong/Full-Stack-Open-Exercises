f632a379305fa499058df614d718baf2d6768249
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-05 16:04:56 +0800

    added exercise 3.2 phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -26,8 +26,15 @@ let persons = [
   }
 ]
 
-app.get('/api/perons', (request, response) => {
+app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(
+    `Phonebook has info for ${persons.length} people<br>${new Date()}`
+  )
+  console.log(request.headers.host, "HERE")
 })
 
 const PORT = 3001
