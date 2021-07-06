61cb0925bec0d247283496df5226c14cc91d13e4
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-06 12:24:00 +0800

    added exercise 3.9 phonebook backend step9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'));
 
