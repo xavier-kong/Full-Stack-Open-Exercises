1dc5a67c9eca428ca6ec3871e3e6deccb82c701a
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-09 18:21:36 +0800

    added exercise 3.14 phonebook database step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -20,55 +20,72 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/info', (request, response) => {
-  response.send(
-    `Phonebook has info for ${persons.length} people<br>${new Date()}`
-  )
-})
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(note => {
-    response.json(note)
+  const note = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => note.id !== id)
 
-  response.status(204).end()
-})
+// app.get('/info', (request, response) => {
+//   response.send(
+//     `Phonebook has info for ${persons.length} people<br>${new Date()}`
+//   )
+// })
 
-const generateId = () => {
-  const newId = Math.floor(Math.random() * 1000000);
-  return newId
-}
+// app.get('/api/persons/:id', (request, response) => {
+//   Person.findById(request.params.id).then(note => {
+//     response.json(note)
+//   })
+// })
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name is missing' 
-    })
-  } else if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number is missing' 
-    })
-  } else if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  } else {
-    const person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number,
-    }
-    persons = persons.concat(person)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter(note => note.id !== id)
+
+//   response.status(204).end()
+// })
+
+// const generateId = () => {
+//   const newId = Math.floor(Math.random() * 1000000);
+//   return newId
+// }
+
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
+//   if (!body.name) {
+//     return response.status(400).json({ 
+//       error: 'name is missing' 
+//     })
+//   } else if (!body.number) {
+//     return response.status(400).json({ 
+//       error: 'number is missing' 
+//     })
+//   } else if (persons.find(person => person.name === body.name)) {
+//     return response.status(400).json({ 
+//       error: 'name must be unique' 
+//     })
+//   } else {
+//     const person = {
+//       id: generateId(),
+//       name: body.name,
+//       number: body.number,
+//     }
+//     persons = persons.concat(person)
   
-    response.json(person)
-  }
-})
+//     response.json(person)
+//   }
+// })
 
 
 const PORT = process.env.PORT
