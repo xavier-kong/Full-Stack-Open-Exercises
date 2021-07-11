f0eae53986c1a2aca9e2c058dc0792ced5707f9b
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-11 15:12:59 +0800

    added exercise 4.2 blog list step2

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,64 +1,17 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'))
 
 morgan.token('req', function(req) {
   return JSON.stringify(req.body)
 })
 
-app.get('/api/blogs', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
