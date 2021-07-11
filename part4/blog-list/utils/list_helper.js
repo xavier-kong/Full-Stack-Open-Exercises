d4af5816c6665f279e236afd0c26e9807e3857ed
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-11 16:37:15 +0800

    added exercise 4.4 helper functions and unit tests step2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,6 +1,18 @@
 const dummy = blogs => 1
 
-module.exports = {
-  dummy
+const totalLikes = blogs => {
+  if (blogs.length == 1) {
+    return blogs[0].likes
+  } else {
+    const red = blogs.reduce((a, b) => a + b.likes, 0)
+    console.log(red)
+    return (
+      red
+    )
+  }
 }
 
+module.exports = {
+  dummy,
+  totalLikes
+}
