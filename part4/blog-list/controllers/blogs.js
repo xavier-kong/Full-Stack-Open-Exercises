8fcd993b0514396b5656e08cf7af48bd0d55b771
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-15 18:09:31 +0800

    added exercisr 4.18-4.19 bloglist expansion, step 6-7

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -8,11 +9,23 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
 
-  const users = await User.find()
-  const user = users[0]
+  const user = await User.findById(decodedToken.id)
 
   if (!body.title || !body.url) {
     response.status(400).end()
