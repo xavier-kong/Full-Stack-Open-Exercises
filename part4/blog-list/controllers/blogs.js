73633fe6094c28d02da692f3b9d5b58700d4e63c
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-16 16:39:29 +0800

    added exercises 4.22*: bloglist expansion, step10

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -16,7 +16,7 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   if (!body.title || !body.url) {
     response.status(400).end()
@@ -37,15 +37,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  // const body = request.body
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  
 
   if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   } 
 
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
   const blog = await Blog.findById(request.params.id)
   if ( blog.user.toString() === user.id.toString()) {
     await Blog.findByIdAndRemove(request.params.id)
