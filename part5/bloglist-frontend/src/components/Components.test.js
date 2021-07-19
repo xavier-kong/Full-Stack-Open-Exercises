2fcbd018729575e1a5b78c18368e8aae61482597
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-19 18:02:08 +0800

    added exercise 5.15*: Blog list tests, step3

--- part5/bloglist-frontend/src/components/Components.test.js
+++ part5/bloglist-frontend/src/components/Components.test.js
@@ -4,23 +4,19 @@ import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  let component
-
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    url: 'www.testurl.com',
-    likes: 5
-  }
-
-  beforeEach(() => {
-    component = render(
-      <Blog key={blog.id} blog={blog} />
-    )
-  }
-  )
 
   test('blog renders the title and author, but not url or likes by default', () => {
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'www.testurl.com',
+      likes: 5
+    }
+
+    const component = render(
+      <Blog key={blog.id} blog={blog}  />
+    )
+
     const div = component.container.querySelector('.testrendertitleauthor')
     expect(div).toHaveTextContent(
       'test title test author'
@@ -30,6 +26,17 @@ describe('<Blog />', () => {
   })
 
   test('show details when view button is clicked', () => {
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'www.testurl.com',
+      likes: 5
+    }
+
+    const component = render(
+      <Blog key={blog.id} blog={blog}  />
+    )
+
     const button = component.getByText('view')
     fireEvent.click(button)
 
@@ -37,4 +44,27 @@ describe('<Blog />', () => {
     expect(details).not.toHaveStyle('display: none')
   })
 
+  test('add like callled twice when like button clicked twice', () => {
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'www.testurl.com',
+      likes: 5
+    }
+
+    const mockLikes = jest.fn()
+
+    const component = render(
+      <Blog key={blog.id} blog={blog}  addLikes={mockLikes}/>
+    )
+
+    const view = component.getByText('view')
+    fireEvent.click(view)
+
+    const like = component.getByText('like')
+    fireEvent.click(like)
+    fireEvent.click(like)
+
+    expect(mockLikes.mock.calls).toHaveLength(2)
+  })
 })
