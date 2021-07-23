9441ee675b819a01d41d68f9b97af8aa24da9a9e
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-23 18:13:10 +0800

    added exercise 6.5*: anecdotes, step3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -20,7 +20,9 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes
+        .sort((a, b) => (a.votes - b.votes))
+        .map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
