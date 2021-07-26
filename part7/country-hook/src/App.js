0c14f002f8e2611c6f9e854a0c334d37ba3b1447
xavier-kong <kongxianchengxavier@gmail.com>
2021-07-26 15:26:14 +0800

    7.7: country hook

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,25 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) {
+      axios
+      .get(`https://restcountries.eu/rest/v2/name/${(name)}?fullText=true`)
+      .then(response => {
+        setCountry({
+          data: response.data[0],
+          found: true
+        })
+      })
+    .catch(error => {
+        setCountry({
+          found: false
+        })
+      })
+    }
+  },
+  [name],
+  )
 
   return country
 }
